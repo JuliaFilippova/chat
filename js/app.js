4eba287b8a7e3afddc050405df4c988baa5169ad
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-08 22:34:40 +0300

    поправила код

--- js/app.js
+++ js/app.js
@@ -1,64 +1,62 @@
 // отправка сообщения в чат
 const sendMessageFunction = () => {
-	if (messageIsEmpty()) return;
-        //Функция которая берет текст из поля ввода
-        let message = $('textarea').val();
-
-        //Формирует сообщение
-        $('#chatbox ul').append(
-        getNewMessageHtml()
-        );
-
-        clearMessageInput();
-
-    };
+    if (messageIsEmpty()) return;
     
-    // после ввода текста отправляем клавишей enter
-    $("#textarea").keyup(e => {
-        //Проверка что нажали enter
-        if(e.key === 'Enter') sendMessageFunction()
-    });
-    // отправка сообщения по кнопке btn-send
-    $("#btn-send").click(sendMessageFunction);
+    //Формирует сообщение
+    $('#chatbox ul').append(
+        getNewMessageHtml()
+    );
+
+    clearMessageInput();
+
+};
+
+// после ввода текста отправляем клавишей enter
+$("#textarea").keyup(e => {
+    //Проверка что нажали enter
+    if (e.key === 'Enter') sendMessageFunction()
+});
+// отправка сообщения по кнопке btn-send
+$("#btn-send").click(sendMessageFunction);
 
 const getNewMessageHtml = () => {
     //Функция которая берет текст из поля ввода
     let message = $('textarea').val();
 
     // использования document.createElement
-	const liItem       = document.createElement('li'),
-	      divMessage   = document.createElement('div'),
-	      spanTime     = document.createElement('span'),
-	      divAvatarBox = document.createElement('div'),
-	      imgAvatar    = document.createElement('img');
-	// Теперь каждому надо дать свой класс и засунуть друг в друга ( изнутри наружу )
+    const liItem = document.createElement('li'),
+        divMessage = document.createElement('div'),
+        spanTime = document.createElement('span'),
+        divAvatarBox = document.createElement('div'),
+        imgAvatar = document.createElement('img');
+    // Теперь каждому надо дать свой класс и засунуть друг в друга ( изнутри наружу )
 
-	// Через setAttribute ставим картинке ссылку. Так можно установить любой аттрибут
-	imgAvatar.setAttribute('src', 'https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg');
+    // Через setAttribute ставим картинке ссылку. Так можно установить любой аттрибут
+    imgAvatar.setAttribute('src', 'https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg');
 
-	// Класс для дива в котором лежит картинка
-	divAvatarBox.classList.add('msg-box__avatar');
+    // Класс для дива в котором лежит картинка
+    divAvatarBox.classList.add('msg-box__avatar');
 
-	// Кладем картинку внутрь дива
-	divAvatarBox.appendChild(imgAvatar);
+    // Кладем картинку внутрь дива
+    divAvatarBox.appendChild(imgAvatar);
 
-	// Класс для span
-	spanTime.classList.add('msg_time');
-	spanTime.innerText = '04:20';
+    // Класс для span
+    spanTime.classList.add('msg_time');
+    spanTime.innerText = '04:20';
 
-	// Класс для div в котором лежит span и div c картинкой
-	divMessage.classList.add('message');
-	// Кладем span и div с картинкой в .message
-	divMessage.innerText = message;
-	divMessage.appendChild(spanTime);
-	divMessage.appendChild(divAvatarBox);
+    // Класс для div в котором лежит span и div c картинкой
+    divMessage.classList.add('message');
+    // Кладем span и div с картинкой в .message
+    divMessage.innerText = message;
+    divMessage.appendChild(spanTime);
+    divMessage.appendChild(divAvatarBox);
 
-	// Класс для li
-	liItem.classList.add('items');
-	// Кладем .message в li
-	liItem.appendChild(divMessage);
+    // Класс для li
+    liItem.classList.add('items');
+    // Кладем .message в li
+    liItem.appendChild(divMessage);
 
-	return liItem;
+    return liItem;
 }
 
 const messageIsEmpty = () => {
@@ -69,7 +67,5 @@ const messageIsEmpty = () => {
 
 const clearMessageInput = () => {
     //Очищаем тут поле ввода после отправки смс
-        $ ('#textarea').val('');
+    $('#textarea').val('');
 }
-
-    
