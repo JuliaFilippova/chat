24fc195b8d8fd617ca8fe5c3a0245534bf5e08f2
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-08 21:44:45 +0300

    Правка кода сообщений

--- js/app.js
+++ js/app.js
@@ -4,10 +4,9 @@ const sendMessageFunction = () => {
         let message = $('textarea').val();
 
         //Формирует сообщение
-        let messageHtml = '<div class="message">' + message + '</div>';
-
-        //Вставляет в место где они выводятся
-        $('#chatbox').append(messageHtml);
+        $('#chatbox ul').append(
+        getNewMessageHtml()
+        );
 
         //очищает полеввода после отправки
         $ ('#textarea').val('');
@@ -20,6 +19,47 @@ const sendMessageFunction = () => {
     });
     // отправка сообщения по кнопке btn-send
     $("#btn-send").click(sendMessageFunction);
+
+const getNewMessageHtml = () => {
+    //Функция которая берет текст из поля ввода
+    let message = $('textarea').val();
+
+    // использования document.createElement
+	const liItem       = document.createElement('li'),
+	      divMessage   = document.createElement('div'),
+	      spanTime     = document.createElement('span'),
+	      divAvatarBox = document.createElement('div'),
+	      imgAvatar    = document.createElement('img');
+	// Теперь каждому надо дать свой класс и засунуть друг в друга ( изнутри наружу )
+
+	// Через setAttribute ставим картинке ссылку. Так можно установить любой аттрибут
+	imgAvatar.setAttribute('src', 'https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg');
+
+	// Класс для дива в котором лежит картинка
+	divAvatarBox.classList.add('msg-box__avatar');
+
+	// Кладем картинку внутрь дива
+	divAvatarBox.appendChild(imgAvatar);
+
+	// Класс для span
+	spanTime.classList.add('msg_time');
+	spanTime.innerText = '04:20';
+
+	// Класс для div в котором лежит span и div c картинкой
+	divMessage.classList.add('message');
+	// Кладем span и div с картинкой в .message
+	divMessage.innerText = message;
+	divMessage.appendChild(spanTime);
+	divMessage.appendChild(divAvatarBox);
+
+	// Класс для li
+	liItem.classList.add('items');
+	// Кладем .message в li
+	liItem.appendChild(divMessage);
+
+	return liItem;
+}
+
 
 
     
